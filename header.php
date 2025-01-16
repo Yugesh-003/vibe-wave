09ce8d6baae641cc6473c933a29170aa1dcc157c
yugesh <a.yugesh2@gmail.com>
2025-01-16 15:42:20 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vibe Wave</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="topnav">
+        <a><img src="img/vibe-wave2.jpg" class="logo" alt="img" /></a>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li class="login"><a href="#login">Login</a></li>
+        </ul>
+      </div>
+      </nav>
